ce5c3588ab9817bb6b1f881debf01a82b154b56f
Manuel Tati <manuel.tati@minfin.gov.ao>
2023-11-11 23:20:15 +0000

    feat:implemented header component

--- src/app/main/shared/components/header.component.ts
+++ src/app/main/shared/components/header.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'header-component',
-  template: ` <p>header works!</p> `,
+  template: `
+    <div class="border-b-[1px] border-neutral-800 p-5">
+      <div class="flex flex-row items-center gap-2">
+        <span
+          *ngIf="showbackArrow"
+          class="material-icons text-white cursor-pointer"
+          (click)="goBack()"
+          >arrow_back</span
+        >
+        <h1 class="text-white text-lg font-semibold">{{ title }}</h1>
+      </div>
+    </div>
+  `,
   styles: [],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Input() showbackArrow: boolean = false;
+  @Input() title: string = '';
+
+  constructor(private router: Router) {}
+
+  goBack = () => this.router.navigate(['/']);
+}
